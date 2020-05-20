7c30a22da69b71b02c7af792c0836ea6f1f8170d
AlecDWalker <walkeralec@hotmail.com>
2020-05-20 14:01:18 +0100

    refactored to use the map method

--- lib/app.rb
+++ lib/app.rb
@@ -1,11 +1,13 @@
 def modulate(array, min = 40, max = 1000)
   raise "Error: impossible range specified" if min > max
   raise "Error: corrupted input (nill values contained)" if array.any?(nil)
-  new_array = []
-  array.each { |wave|
-    wave = min if wave < min
-    wave = max if wave > max
-    new_array << wave
+  return array.map { |wave|
+    if wave < min
+      wave = min
+    elsif wave > max
+      wave = max
+    else
+      wave = wave
+    end
   }
-  return new_array
 end
